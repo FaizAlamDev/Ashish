e7dbb242f7c1cf1f57742abba9cc55b1b28d7142
Faiz Alam <faizalam9883@gmail.com>
2023-03-13 02:41:00 +0530

    add loading

--- frontend/src/components/AllForms.js
+++ frontend/src/components/AllForms.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const AllForms = () => {
   const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const request = axios.get("/allForms");
     request.then((res) => {
@@ -10,31 +12,62 @@ const AllForms = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (details.length > 0) {
+      setLoading(false);
+    }
+  }, [details]);
+
+  const Loading = () => {
+    return (
+      <div
+        style={{
+          height: "400px",
+          width: "800px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <h1>Loading...</h1>
+      </div>
+    );
+  };
+
   return (
-    <div className="table">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date Of Birth</th>
-            <th>Email</th>
-            <th>Contact No.</th>
-          </tr>
-        </thead>
-        <tbody>
-          {details.map((val, key) => {
-            return (
-              <tr key={key}>
-                <td>{val.name}</td>
-                <td>{val.date}</td>
-                <td>{val.email}</td>
-                <td>{val.contact}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Date Of Birth</th>
+                  <th>Email</th>
+                  <th>Contact No.</th>
+                </tr>
+              </thead>
+              <tbody>
+                {details.map((val, key) => {
+                  return (
+                    <tr key={key}>
+                      <td>{val.name}</td>
+                      <td>{val.date}</td>
+                      <td>{val.email}</td>
+                      <td>{val.contact}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
